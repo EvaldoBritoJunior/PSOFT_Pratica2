34d67b9110a41eb441a441b3d1e09605e0d0b823
EvaldoBritoJunior <111549934+EvaldoBritoJunior@users.noreply.github.com>
2023-04-03 09:49:38 -0300

    Delete src directory

--- src/main/java/com/ufcg/psoft/mercadofacil/service/ProdutoAlterarService.java
+++
@@ -1,7 +0,0 @@
-package com.ufcg.psoft.mercadofacil.service;
-
-import com.ufcg.psoft.mercadofacil.model.Produto;
-
-public interface ProdutoAlterarService {
-    Produto alterar(Produto produtoAlterado);
-}
