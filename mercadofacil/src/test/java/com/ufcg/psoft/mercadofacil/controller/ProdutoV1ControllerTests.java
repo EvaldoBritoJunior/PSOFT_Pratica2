ea14ad4072f7ba408c6a7a8a22898eaab9cd57b2
EvaldoBritoJunior <111549934+EvaldoBritoJunior@users.noreply.github.com>
2023-04-05 13:23:22 -0300

    Add files via upload

--- mercadofacil/src/test/java/com/ufcg/psoft/mercadofacil/controller/ProdutoV1ControllerTests.java
+++ mercadofacil/src/test/java/com/ufcg/psoft/mercadofacil/controller/ProdutoV1ControllerTests.java
@@ -3,6 +3,7 @@ package com.ufcg.psoft.mercadofacil.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ufcg.psoft.mercadofacil.model.Produto;
 import com.ufcg.psoft.mercadofacil.repository.ProdutoRepository;
+import jakarta.servlet.ServletException;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -11,6 +12,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -63,18 +65,149 @@ public class ProdutoV1ControllerTests {
             assertEquals(resultado.getNome(), "Nome Produto Alterado");
         }
 
+        @Test
+        @DisplayName("Quando altero o nome do fabricante com dados válidos")
+        void alterarFabricanteDoProduto() throws Exception {
+            /* AAA Pattern */
+            //Arrange
+            produto.setFabricante("Nome Fabricante Alterado");
+            //Act
+            String responseJsonString = driver.perform(put("/v1/produtos/" + produto.getId())
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(produto)))
+                    .andExpect(status().isOk())
+                    .andDo(print())
+                    .andReturn().getResponse().getContentAsString();
+
+            Produto resultado = objectMapper.readValue(responseJsonString, Produto.ProdutoBuilder.class).build();
+            //Assert
+            assertEquals("Nome Fabricante Alterado", resultado.getFabricante());
+        }
+
+        @Test
+        @DisplayName("Quando altero o codigo de barra com dados válidos")
+        void alterarCodigoDoProduto() throws Exception {
+            /* AAA Pattern */
+            //Arrange
+            produto.setCodigoBarra("7899137500117");
+            //Act
+            String responseJsonString = driver.perform(put("/v1/produtos/" + produto.getId())
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(produto)))
+                    .andExpect(status().isOk())
+                    .andDo(print())
+                    .andReturn().getResponse().getContentAsString();
+
+            Produto resultado = objectMapper.readValue(responseJsonString, Produto.ProdutoBuilder.class).build();
+            //Assert
+            assertEquals("7899137500117", resultado.getCodigoBarra());
+        }
+
     }
 
     @Nested
     @DisplayName("Conjunto de casos de verificação da regra sobre o preço")
     class ProdutoValidacaoRegrasDoPreco {
-        // Implementar os testes aqui
+
+        @Test
+        @DisplayName("Quando altero o preço do produto com dados válidos")
+        void alterarPrecoDoProduto() throws Exception {
+            /* AAA Pattern */
+            //Arrange
+            produto.setPreco(500.00);
+            //Act
+            String responseJsonString = driver.perform(put("/v1/produtos/" + produto.getId())
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(produto)))
+                    .andExpect(status().isOk())
+                    .andDo(print())
+                    .andReturn().getResponse().getContentAsString();
+
+            Produto resultado = objectMapper.readValue(responseJsonString, Produto.ProdutoBuilder.class).build();
+            //Assert
+            assertEquals(500.00, resultado.getPreco());
+        }
+
+        @Test
+        @DisplayName("Quando o preço é menor ou igual a zero")
+        void precoMenorIgualAZero() {
+            //Arrange
+            produto.setPreco(0.0);
+            //Act
+            ServletException thrown = assertThrows(
+                    ServletException.class,
+                    () -> driver.perform(put("/v1/produtos/" + produto.getId())
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .content(objectMapper.writeValueAsString(produto)))
+                            .andExpect(status().isOk())
+                            .andDo(print())
+                            .andReturn().getResponse().getContentAsString()
+            );
+            //Assert
+            assertEquals("Request processing failed: java.lang.RuntimeException: Preco invalido!", thrown.getMessage());
+        }
+
     }
 
     @Nested
     @DisplayName("Conjunto de casos de verificação da validação do código de barras")
     class ProdutoValidacaoCodigoDeBarras {
-        // Implementar os testes aqui
+        @Test
+        @DisplayName("Quando o codigo de barras tem digito verificador invalido")
+        void digitoVerificadorInvalido() {
+            //Arrange
+            produto.setCodigoBarra("7899137500104");
+            //Act
+            ServletException thrown = assertThrows(
+                    ServletException.class,
+                    () -> driver.perform(put("/v1/produtos/" + produto.getId())
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .content(objectMapper.writeValueAsString(produto)))
+                            .andExpect(status().isOk())
+                            .andDo(print())
+                            .andReturn().getResponse().getContentAsString()
+            );
+            //Assert
+            assertEquals("Request processing failed: java.lang.RuntimeException: Codigo de barra com digito verificador incorreto!", thrown.getMessage());
+        }
+
+        @Test
+        @DisplayName("Quando o codigo de barras tem pais invalido")
+        void codigoPaisInvalido() {
+            //Arrange
+            produto.setCodigoBarra("9879137500104");
+            //Act
+            ServletException thrown = assertThrows(
+                    ServletException.class,
+                    () -> driver.perform(put("/v1/produtos/" + produto.getId())
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .content(objectMapper.writeValueAsString(produto)))
+                            .andExpect(status().isOk())
+                            .andDo(print())
+                            .andReturn().getResponse().getContentAsString()
+            );
+            //Assert
+            assertEquals("Request processing failed: java.lang.RuntimeException: Codigo de barra com país errado", thrown.getMessage());
+        }
+
+        @Test
+        @DisplayName("Quando o codigo de barras tem empresa invalida")
+        void codigoEmpresaInvalida() {
+            //Arrange
+            produto.setCodigoBarra("7895137500104");
+            //Act
+            ServletException thrown = assertThrows(
+                    ServletException.class,
+                    () -> driver.perform(put("/v1/produtos/" + produto.getId())
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .content(objectMapper.writeValueAsString(produto)))
+                            .andExpect(status().isOk())
+                            .andDo(print())
+                            .andReturn().getResponse().getContentAsString()
+            );
+            //Assert
+            assertEquals("Request processing failed: java.lang.RuntimeException: Codigo de barra com empresa errada!", thrown.getMessage());
+        }
     }
 
 }
